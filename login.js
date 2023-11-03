eb1db0d78b5010c69b54e9b4856b45011044619b
Cedric Chang <cedric.chang00@gmail.com>
2023-11-03 17:32:35 +0100

    Init frontend

---
+++ login.js
@@ -0,0 +1,44 @@
+// Insert your code here
+document.querySelector('#register').addEventListener('click', function() {
+    const userName = document.querySelector('#registerName').value;
+    const userEmail = document.querySelector('#registerEmail').value;
+    const userPassword = document.querySelector('#registerPassword').value;
+    let data = {
+        name: userName,
+        email: userEmail,
+        mdp: userPassword,
+    }
+    fetch('http://localhost:3000/users/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify(data),
+    }).then(response => response.json()).then(data => {
+        if (data.result) {
+            window.location.assign('index.html');
+            console.log('Success');
+        } else {
+            console.log('Fail to register');
+        }
+    })
+});
+
+document.querySelector('#connection').addEventListener('click', function() {
+    const userEmail = document.querySelector('#connectionEmail').value;
+    const userPassword = document.querySelector('#connectionPassword').value;
+    let body = {
+        email: userEmail,
+        mdp: userPassword,
+    }
+    fetch('http://localhost:3000/users/signin', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({ body }),
+    }).then(response => response.json()).then(data => {
+        if (data.result) {
+            window.location.assign('index.html');
+            console.log('Success');
+        } else {
+            console.log("Invalid password or email");
+        }
+    })
+});
